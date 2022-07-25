02207843954aeae4fa3ab4999983cd5a7b278492
amen365 <60744064+amen365@users.noreply.github.com>
2022-07-25 14:17:25 -0400

    Update HelloGoodbye.java

--- Tutorial/HelloGoodbye.java
+++ Tutorial/HelloGoodbye.java
@@ -1,3 +1,14 @@
+/******************************************************************************
+ *  Compilation:  javac HelloGoodbye.java
+ *  Execution:    java HelloGoodbye Kevin Bob
+ *  
+ *  Reads in two Names command-line arguments and prints "Hello Kevin and Bob" and "Goodbyu Kevin and Bob"
+ *  Write a program HelloGoodbye.java that takes two names as command-line arguments and prints hello and 
+ *  goodbye messages as shown below (with the names for the hello message in the same order as the command-line 
+ *  arguments and with the names for the goodbye message in reverse order)
+ *  % java HelloGoodbye Kevin Bob
+ ******************************************************************************/
+
 public class HelloGoodbye {
     public static void main(String[] args){
         System.out.println("Hello");
