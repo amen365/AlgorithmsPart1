e3ba74a4dbe376f2580e55fb3251adfffd11de22
amen365 <60744064+amen365@users.noreply.github.com>
2022-07-25 14:00:23 -0400

    Create HelloGoodbye.java

---
+++ HelloGoodbye.java
@@ -0,0 +1,8 @@
+public class HelloGoodbye {
+    public static void main(String[] args){
+        System.out.println("Hello");
+        System.out.println(args[0]);
+        System.out.println("and");
+    }
+    
+}
